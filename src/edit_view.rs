da066f927d9d055bb2997f54e87b4ad7701e7225
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2018-10-31 16:31:28 +0100

    Use druid in xi-win (#58)
    * Refactor for druid structure
    
    Lots of changes to accommodate the structure needed for xi-win to be
    implemented on top of druid
    
    Most notable changes:
    `AppDispatcher` handles notifications from core, and also forwards menu
    events to views.
    `AppState` keeps track of the currently focused views, and is intended
    as a reference for accessing views by core view_id values.
    `EditView` has a weak reference to Core, allowing it to send cmds
    directly.
    * Store IdleHandle in ViewState
    
    * 🎉 New author
    
    * Use FileDialogs in menu listeners
    
    Added save, save_as and open

--- src/edit_view.rs
+++ src/edit_view.rs
@@ -16,6 +16,8 @@
 
 use std::cmp::min;
 use std::ops::Range;
+use std::any::Any;
+use std::sync::{Mutex, Weak};
 
 use serde_json::Value;
 
@@ -27,25 +29,46 @@ use direct2d::RenderTarget;
 use directwrite;
 use directwrite::TextFormat;
 
-use xi_win_shell::paint::PaintCtx;
-use xi_win_shell::window::{M_ALT, M_CTRL, M_SHIFT, MouseButton, MouseType};
+use xi_win_shell::window::{M_ALT, M_CTRL, M_SHIFT, MouseButton};
 
-use MainWin;
+use xi_win_ui::UiInner;
+use xi_win_ui::widget::Widget;
 
+use xi_win_ui::{BoxConstraints, Geometry, LayoutResult};
+use xi_win_ui::{HandlerCtx, Id, LayoutCtx, MouseEvent, PaintCtx, KeyEvent};
+use xi_win_ui::{KeyVariant};
+
+use rpc::Core;
 use linecache::LineCache;
 use textline::TextLine;
 
+/// The commands the EditView widget accepts through `poke`.
+pub enum EditViewCommands {
+    ViewId(String),
+    ApplyUpdate(Value),
+    ScrollTo(usize),
+    Core(Weak<Mutex<Core>>),
+    Undo,
+    Redo,
+    UpperCase,
+    LowerCase,
+    Transpose,
+    AddCursorAbove,
+    AddCursorBelow,
+    SingleSelection,
+    SelectAll,
+}
+
 /// State and behavior for one editor view.
 pub struct EditView {
-    // Note: these public fields should be properly encapsulated.
-    pub view_id: String,
-    pub filename: Option<String>,
+    view_id: String,
     line_cache: LineCache,
     dwrite_factory: directwrite::Factory,
     resources: Option<Resources>,
     scroll_offset: f32,
     size: (f32, f32),  // in px units
     viewport: Range<usize>,
+    core: Weak<Mutex<Core>>,
 }
 
 struct Resources {
@@ -59,18 +82,135 @@ const TOP_PAD: f32 = 6.0;
 const LEFT_PAD: f32 = 6.0;
 const LINE_SPACE: f32 = 17.0;
 
+impl Widget for EditView {
+    fn paint(&mut self, paint_ctx: &mut PaintCtx, geom: &Geometry) {
+        // todo: Cache resources, and flush cache when the render target is re-created.
+        self.size = geom.size;
+        let resources = self.create_resources(paint_ctx);
+        let rt = paint_ctx.render_target();
+        let rect = RectF::from((0.0, 0.0, self.size.0, self.size.1));
+        rt.fill_rectangle(rect, &resources.bg);
+
+        let first_line = self.y_to_line(0.0);
+        let last_line = min(self.y_to_line(self.size.1) + 1, self.line_cache.height());
+
+        let x0 = LEFT_PAD;
+        let mut y = self.line_to_content_y(first_line) - self.scroll_offset;
+        for line_num in first_line..last_line {
+            if let Some(textline) = self.get_text_line(line_num) {
+                textline.draw_bg(rt, x0, y, &resources.sel);
+            }
+            y += LINE_SPACE;
+        }
+        let mut y = self.line_to_content_y(first_line) - self.scroll_offset;
+        for line_num in first_line..last_line {
+            if let Some(textline) = self.get_text_line(line_num) {
+                textline.draw_text(rt, x0, y, &resources.fg);
+                textline.draw_cursor(rt, x0, y, &resources.fg);
+            }
+            y += LINE_SPACE;
+        }
+        self.resources = Some(resources);
+    }
+
+    fn layout(&mut self, bc: &BoxConstraints, _children: &[Id], _size: Option<(f32, f32)>,
+        _ctx: &mut LayoutCtx) -> LayoutResult
+    {
+        LayoutResult::Size(bc.constrain((0.0, 0.0)))
+    }
+
+    fn mouse(&mut self, event: &MouseEvent, _ctx: &mut HandlerCtx) -> bool { 
+        let MouseEvent { x, y, mods: _, which, count } = *event;
+        if which == MouseButton::Left && count == 1 {
+            let (line, col) = self.xy_to_line_col(x, y);
+            let params = json!({
+                "ty": "point_select",
+                "line": line,
+                "col": col,
+            });
+            self.send_edit_cmd("gesture", &params);
+        }
+        false
+    }
+
+    fn poke(&mut self, payload: &mut Any, ctx: &mut HandlerCtx) -> bool { 
+        if let Some(cmd) = payload.downcast_ref::<EditViewCommands>() {
+            match cmd {
+                EditViewCommands::ViewId(view_id) => {
+                    self.view_id = view_id.to_string();
+                }
+                EditViewCommands::ApplyUpdate(update) => {
+                    self.apply_update(&update);
+                }
+                EditViewCommands::ScrollTo(line) => {
+                    self.scroll_to(*line);
+                }
+                EditViewCommands::Core(core) => {
+                    self.core = core.clone();
+                }
+                EditViewCommands::Undo => {
+                    self.send_action("undo");
+                }
+                EditViewCommands::Redo => {
+                    self.send_action("redo");
+                }
+                EditViewCommands::UpperCase => {
+                    self.send_action("uppercase");
+                }
+                EditViewCommands::LowerCase => {
+                    self.send_action("lowercase");
+                }
+                EditViewCommands::Transpose => {
+                    self.send_action("transpose");
+                }
+                EditViewCommands::AddCursorAbove => {
+                    self.add_cursor_above();
+                }
+                EditViewCommands::AddCursorBelow => {
+                    self.add_cursor_below();
+                }
+                EditViewCommands::SingleSelection => {
+                    self.single_selection();
+                }
+                EditViewCommands::SelectAll => {
+                    self.select_all();
+                }
+            }
+            // TODO: Finer grained invalidation
+            ctx.invalidate();
+        }
+        true
+    }
+
+    fn key(&mut self, event: &KeyEvent, _ctx: &mut HandlerCtx) -> bool {
+        match event.key {
+            KeyVariant::Vkey(vk) => {
+                self.keydown(vk, event.mods);
+            }
+            KeyVariant::Char(ch) => {
+                self.char(ch as u32, event.mods);
+            }
+        } 
+        true
+    }
+}
+
 impl EditView {
     pub fn new() -> EditView {
         EditView {
             view_id: "".into(),
-            filename: None,
             line_cache: LineCache::new(),
             dwrite_factory: directwrite::Factory::new().unwrap(),
             resources: None,
             scroll_offset: 0.0,
             size: (0.0, 0.0),
             viewport: 0..0,
+            core: Default::default(),
         }
+    }
+
+    pub fn ui(self, ctx: &mut UiInner) -> Id {
+        ctx.add(self, &[])
     }
 
     fn create_resources(&mut self, p: &mut PaintCtx) -> Resources {
@@ -101,36 +241,6 @@ impl EditView {
         self.line_cache = LineCache::new();
     }
 
-    pub fn render(&mut self, p: &mut PaintCtx) {
-        if self.resources.is_none() {
-            self.resources = Some(self.create_resources(p));
-        }
-        let resources = &self.resources.as_ref().unwrap();
-        let rt = p.render_target();
-        let rect = RectF::from((0.0, 0.0, self.size.0, self.size.1));
-        rt.fill_rectangle(rect, &resources.bg);
-
-        let first_line = self.y_to_line(0.0);
-        let last_line = min(self.y_to_line(self.size.1) + 1, self.line_cache.height());
-
-        let x0 = LEFT_PAD;
-        let mut y = self.line_to_content_y(first_line) - self.scroll_offset;
-        for line_num in first_line..last_line {
-            if let Some(textline) = self.get_text_line(line_num) {
-                textline.draw_bg(rt, x0, y, &resources.sel);
-            }
-            y += LINE_SPACE;
-        }
-        let mut y = self.line_to_content_y(first_line) - self.scroll_offset;
-        for line_num in first_line..last_line {
-            if let Some(textline) = self.get_text_line(line_num) {
-                textline.draw_text(rt, x0, y, &resources.fg);
-                textline.draw_cursor(rt, x0, y, &resources.fg);
-            }
-            y += LINE_SPACE;
-        }
-    }
-
     // signature will change when we start caching
     fn get_text_line(&self, line_num: usize) -> Option<TextLine> {
         self.line_cache.get_line(line_num).map(|line| {
@@ -139,51 +249,64 @@ impl EditView {
         })
     }
 
-    pub fn set_view_id(&mut self, view_id: &str) {
-        self.view_id = view_id.into();
-    }
-
     pub fn apply_update(&mut self, update: &Value) {
         self.line_cache.apply_update(update);
         self.constrain_scroll();
     }
 
-    pub fn char(&self, ch: u32, _mods: u32, win: &MainWin) {
+    pub fn char(&self, ch: u32, _mods: u32) {
         if let Some(c) = ::std::char::from_u32(ch) {
             if ch >= 0x20 {
                 // Don't insert control characters
                 let params = json!({"chars": c.to_string()});
-                self.send_edit_cmd("insert", &params, win);
+                self.send_edit_cmd("insert", &params);
             }
         }
     }
 
-    fn send_edit_cmd(&self, method: &str, params: &Value, win: &MainWin) {
-        win.send_edit_cmd(method, params, &self.view_id);
+    fn send_edit_cmd(&self, method: &str, params: &Value) {
+        let edit_params = json!({
+            "method": method,
+            "params": params,
+            "view_id": &self.view_id,
+        });
+        self.send_notification("edit", &edit_params);
+    }
+
+    fn send_notification(&self, method: &str, params: &Value) {
+        if let Some(ref core) = self.core.upgrade() {
+            core.lock().unwrap().send_notification(method, params);
+            // NOTE: For debugging, could be replaced by trace logging
+            // println!("fe->core: {}", json!({
+            //     "method": method,
+            //     "params": params,
+            // }));
+        } else {
+            // TODO: queue pending
+        }
     }
 
     /// Sends a simple action with no parameters
-    fn send_action(&self, method: &str, win: &MainWin) {
-        self.send_edit_cmd(method, &json!([]), win);
+    fn send_action(&self, method: &str) {
+        self.send_edit_cmd(method, &json!([]));
     }
 
-    pub fn keydown(&mut self, vk_code: i32, mods: u32, win: &MainWin) -> bool {
+    pub fn keydown(&mut self, vk_code: i32, mods: u32) -> bool {
         // Handle special keys here
         match vk_code {
             VK_RETURN => {
                 // TODO: modifiers are variants of open
-                self.send_action("insert_newline", win);
+                self.send_action("insert_newline");
             }
             VK_TAB => {
                 // TODO: modified versions
-                self.send_action("insert_tab", win);
+                self.send_action("insert_tab");
             }
             VK_UP => {
                 if mods == M_CTRL {
                     self.scroll_offset -= LINE_SPACE;
                     self.constrain_scroll();
-                    self.update_viewport(win);
-                    win.invalidate();
+                    self.update_viewport();
                 } else {
                     let action = if mods == M_CTRL | M_ALT {
                         "add_selection_above"
@@ -191,22 +314,21 @@ impl EditView {
                         s(mods, "move_up", "move_up_and_modify_selection")
                     };
                     // TODO: swap line up is ctrl + shift
-                    self.send_action(action, win);
+                    self.send_action(action);
                 }
             }
             VK_DOWN => {
                 if mods == M_CTRL {
                     self.scroll_offset += LINE_SPACE;
                     self.constrain_scroll();
-                    self.update_viewport(win);
-                    win.invalidate();
+                    self.update_viewport();
                 } else {
                     let action = if mods == M_CTRL | M_ALT {
                         "add_selection_below"
                     } else {
                         s(mods, "move_down", "move_down_and_modify_selection")
                     };
-                    self.send_action(action, win);
+                    self.send_action(action);
                 }
             }
             VK_LEFT => {
@@ -216,7 +338,7 @@ impl EditView {
                 } else {
                     s(mods, "move_left", "move_left_and_modify_selection")
                 };
-                self.send_action(action, win);
+                self.send_action(action);
             }
             VK_RIGHT => {
                 // TODO: there is a subtle distinction between alt and ctrl
@@ -225,15 +347,15 @@ impl EditView {
                 } else {
                     s(mods, "move_right", "move_right_and_modify_selection")
                 };
-                self.send_action(action, win);
+                self.send_action(action);
             }
             VK_PRIOR => {
                 self.send_action(s(mods, "scroll_page_up",
-                    "page_up_and_modify_selection"), win);
+                    "page_up_and_modify_selection"));
             }
             VK_NEXT => {
                 self.send_action(s(mods, "scroll_page_down",
-                    "page_down_and_modify_selection"), win);
+                    "page_down_and_modify_selection"));
             }
             VK_HOME => {
                 let action = if (mods & M_CTRL) != 0 {
@@ -243,7 +365,7 @@ impl EditView {
                     s(mods, "move_to_left_end_of_line",
                         "move_to_left_end_of_line_and_modify_selection")
                 };
-                self.send_action(action, win);
+                self.send_action(action);
             }
             VK_END => {
                 let action = if (mods & M_CTRL) != 0 {
@@ -253,10 +375,10 @@ impl EditView {
                     s(mods, "move_to_right_end_of_line",
                         "move_to_right_end_of_line_and_modify_selection")
                 };
-                self.send_action(action, win);
+                self.send_action(action);
             }
             VK_ESCAPE => {
-                self.send_action("cancel_operation", win);
+                self.send_action("cancel_operation");
             }
             VK_BACK => {
                 let action = if (mods & M_CTRL) != 0 {
@@ -265,7 +387,7 @@ impl EditView {
                 } else {
                     "delete_backward"
                 };
-                self.send_action(action, win);
+                self.send_action(action);
             }
             VK_DELETE => {
                 let action = if (mods & M_CTRL) != 0 {
@@ -274,12 +396,12 @@ impl EditView {
                     // TODO: shift-delete should be "delete line"
                     "delete_forward"
                 };
-                self.send_action(action, win);
+                self.send_action(action);
             }
             VK_OEM_4 => {
                 // generally '[' key, but might vary on non-US keyboards
                 if mods == M_CTRL {
-                    self.send_action("outdent", win);
+                    self.send_action("outdent");
                 } else {
                     return false
                 }
@@ -287,7 +409,7 @@ impl EditView {
             VK_OEM_6 => {
                 // generally ']' key, but might vary on non-US keyboards
                 if mods == M_CTRL {
-                    self.send_action("indent", win);
+                    self.send_action("indent");
                 } else {
                     return false
                 }
@@ -300,73 +422,36 @@ impl EditView {
     }
 
     // Commands
-
-    pub fn undo(&mut self, win: &MainWin) {
-        self.send_action("undo", win);
-    }
-
-    pub fn redo(&mut self, win: &MainWin) {
-        self.send_action("redo", win);
-    }
-
-    pub fn upper_case(&mut self, win: &MainWin) {
-        self.send_action("uppercase", win);
-    }
-
-    pub fn lower_case(&mut self, win: &MainWin) {
-        self.send_action("lowercase", win);
-    }
-
-    pub fn transpose(&mut self, win: &MainWin) {
-        self.send_action("transpose", win);
-    }
-
-    pub fn add_cursor_above(&mut self, win: &MainWin) {
+    pub fn add_cursor_above(&mut self) {
         // Note: some subtlety around find, the escape key cancels it, but the menu
         // shouldn't.
-        self.send_action("add_selection_above", win);
+        self.send_action("add_selection_above");
     }
 
-    pub fn add_cursor_below(&mut self, win: &MainWin) {
+    pub fn add_cursor_below(&mut self) {
         // Note: some subtlety around find, the escape key cancels it, but the menu
         // shouldn't.
-        self.send_action("add_selection_below", win);
+        self.send_action("add_selection_below");
     }
 
-    pub fn single_selection(&mut self, win: &MainWin) {
+    pub fn single_selection(&mut self) {
         // Note: some subtlety around find, the escape key cancels it, but the menu
         // shouldn't.
-        self.send_action("cancel_operation", win);
+        self.send_action("cancel_operation");
     }
 
-    pub fn select_all(&mut self, win: &MainWin) {
+    pub fn select_all(&mut self) {
         // Note: some subtlety around find, the escape key cancels it, but the menu
         // shouldn't.
-        self.send_action("select_all", win);
+        self.send_action("select_all");
     }
 
-    pub fn mouse(&self, x: f32, y: f32, mods: u32, which: MouseButton, ty: MouseType,
-        win: &MainWin)
-    {
-        if which == MouseButton::Left && ty == MouseType::Down {
-            let (line, col) = self.xy_to_line_col(x, y);
-            let params = json!({
-                "ty": "point_select",
-                "line": line,
-                "col": col,
-            });
-            self.send_edit_cmd("gesture", &params, win);
-            println!("{},{} (line {}) {} {:?} {:?}", x, y, line, mods, which, ty);
-        }
-    }
-
-    pub fn mouse_wheel(&mut self, delta: i32, _mods: u32, win: &MainWin) {
+    pub fn mouse_wheel(&mut self, delta: i32, _mods: u32) {
         // TODO: scale properly, taking SPI_GETWHEELSCROLLLINES into account
         let scroll_scaling = 0.5;
         self.scroll_offset -= (delta as f32) * scroll_scaling;
         self.constrain_scroll();
-        self.update_viewport(win);
-        win.handle.borrow().invalidate();
+        self.update_viewport();
     }
 
     fn constrain_scroll(&mut self) {
@@ -405,13 +490,13 @@ impl EditView {
         TOP_PAD + (line as f32) * LINE_SPACE
     }
 
-    fn update_viewport(&mut self, win: &MainWin) {
+    fn update_viewport(&mut self) {
         let first_line = self.y_to_line(0.0);
         let last_line = first_line + ((self.size.1 / LINE_SPACE).floor() as usize) + 1;
         let viewport = first_line..last_line;
         if viewport != self.viewport {
             self.viewport = viewport;
-            self.send_edit_cmd("scroll", &json!([first_line, last_line]), win);
+            self.send_edit_cmd("scroll", &json!([first_line, last_line]));
         }
     }
 
